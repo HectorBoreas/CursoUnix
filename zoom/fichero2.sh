06d1320a4069fc891f01a2a57a2c916e48c468e1
HectorBoreas <hector_pp_18@yahoo.es>
2024-10-10 11:26:08 +0000

    jueves

--- zoom/fichero2.sh
+++ zoom/fichero2.sh
@@ -15,15 +15,24 @@ fi
 contador=0
 # Leer el archivo línea por línea
 while read -r linea; do	
-	if [ $contador -eq 0 ]; then
+	if [ $contador -eq 0 ] || [  ]; then
 		usuario=$linea	
-		
+		echo $usuario
 	fi
 	if [ $contador -eq 1 ]; then
 		password=$linea	
-		sudo useradd -m "$usuario" && echo "$usuario:$password" | sudo chpasswd
+#		sudo useradd -m "$usuario" --badname && echo "$usuario:$password" | sudo chpasswd
+		echo $password
+	fi
+	if [ $contador -eq 2 ]; then
+		permiso=$linea	
+		echo $permiso
+#		sudo chmod
+	fi
+	if [ $contador -eq 3 ]; then
+		contador=$((contador-4))
 	fi
 	contador=$((contador+1))
 	
 done < $1
-echo $usuario $password
+#echo $usuario $linea $permiso
